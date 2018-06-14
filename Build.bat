60c75598e02b9aa1368c389ffa35a75582bdda16
raywu <raywu0301@gmail.com>
2018-06-15 00:23:22 +0800

    build success

--- Build.bat
+++ Build.bat
@@ -1,1 +1,7 @@
+rem Aptio 4.x Build Environment Settings
+rem @set WINDDK_DIR1=%AAEON_WINDDK_DIR%\7600.16385.1\x86\x86
+rem @set WINDDK_DIR2=%AAEON_WINDDK_DIR%\7600.16385.1\x86
+rem @set CCX86DIR_DIR=%AAEON_WINDDK_DIR%\7600.16385.1\x86\x86
+rem @set CCX64DIR_DIR=%AAEON_WINDDK_DIR%\7600.16385.1\x86\amd64
+
 nmake /nologo /f build.mak %*
